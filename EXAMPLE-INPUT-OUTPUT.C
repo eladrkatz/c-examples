09b722e78019b2b4e666214c239011ab9e062473
Elad Katz <eladkt@gmail.com>
2016-11-29 02:15:41 +0200

    first working c program

--- EXAMPLE-INPUT-OUTPUT.C
+++ EXAMPLE-INPUT-OUTPUT.C
@@ -1,16 +1,18 @@
 
-include "stdio"
+#include <stdio.h>
 
 int main()
 {
     int a, b, sum;
+
+    printf("please insert two numbers to add");
 
     scanf("%d", &a);
     scanf("%d", &b);
 
     sum = a + b;
 
-    printf("%d", sum);
+    printf("%d\n", sum);
 
     return 0;
 }
